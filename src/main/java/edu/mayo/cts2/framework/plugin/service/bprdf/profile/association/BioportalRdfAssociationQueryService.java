ac0a03d7b1a23585483c9a4bf7a8002a7a689b4c
kpradip <kpradip@github.com>
2012-02-21 16:45:08 -0600

    Committing the association query service

--- src/main/java/edu/mayo/cts2/framework/plugin/service/bprdf/profile/association/BioportalRdfAssociationQueryService.java
+++ src/main/java/edu/mayo/cts2/framework/plugin/service/bprdf/profile/association/BioportalRdfAssociationQueryService.java
@@ -1,7 +1,14 @@
 package edu.mayo.cts2.framework.plugin.service.bprdf.profile.association;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
 
 import edu.mayo.cts2.framework.model.association.Association;
 import edu.mayo.cts2.framework.model.association.AssociationDirectoryEntry;
@@ -9,17 +16,19 @@ import edu.mayo.cts2.framework.model.association.GraphNode;
 import edu.mayo.cts2.framework.model.association.types.GraphDirection;
 import edu.mayo.cts2.framework.model.association.types.GraphFocus;
 import edu.mayo.cts2.framework.model.command.Page;
+import edu.mayo.cts2.framework.model.command.ResolvedFilter;
 import edu.mayo.cts2.framework.model.command.ResolvedReadContext;
-import edu.mayo.cts2.framework.model.core.FormatReference;
-import edu.mayo.cts2.framework.model.core.NamespaceReference;
-import edu.mayo.cts2.framework.model.core.OpaqueData;
-import edu.mayo.cts2.framework.model.core.PredicateReference;
+import edu.mayo.cts2.framework.model.core.ModelAttributeReference;
 import edu.mayo.cts2.framework.model.core.SortCriteria;
-import edu.mayo.cts2.framework.model.core.SourceReference;
 import edu.mayo.cts2.framework.model.directory.DirectoryResult;
 import edu.mayo.cts2.framework.model.entity.EntityDirectoryEntry;
 import edu.mayo.cts2.framework.model.entity.EntityList;
+import edu.mayo.cts2.framework.model.service.core.NameOrURI;
+import edu.mayo.cts2.framework.plugin.service.bprdf.dao.RdfDao;
+import edu.mayo.cts2.framework.plugin.service.bprdf.dao.id.CodeSystemName;
 import edu.mayo.cts2.framework.plugin.service.bprdf.profile.AbstractQueryService;
+import edu.mayo.cts2.framework.plugin.service.bprdf.profile.VariableQueryBuilder;
+import edu.mayo.cts2.framework.plugin.service.bprdf.profile.VariableQueryBuilder.VariableQuery;
 import edu.mayo.cts2.framework.plugin.service.bprdf.profile.VariableTiedModelAttributeReference;
 import edu.mayo.cts2.framework.service.command.restriction.AssociationQueryServiceRestrictions;
 import edu.mayo.cts2.framework.service.command.restriction.EntityDescriptionQueryServiceRestrictions;
@@ -28,71 +37,19 @@ import edu.mayo.cts2.framework.service.profile.association.AssociationQuery;
 import edu.mayo.cts2.framework.service.profile.association.AssociationQueryService;
 import edu.mayo.cts2.framework.service.profile.entitydescription.EntityDescriptionQuery;
 import edu.mayo.cts2.framework.service.profile.entitydescription.name.EntityDescriptionReadId;
-
+@Component
 public class BioportalRdfAssociationQueryService extends AbstractQueryService implements  AssociationQueryService{
+	private final static String ASSOCIATION_NAMESPACE = "association";
+	private final static String GET_ASSOCIATION_SUMMARIES = "getAssociationDirectoryEntrySummaries";
+	
+	private final static String LIMIT = "limit";
+	private final static String OFFSET = "offset";
 
-	/* (non-Javadoc)
-	 * @see edu.mayo.cts2.framework.service.profile.BaseService#getServiceName()
-	 */
-	@Override
-	public String getServiceName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	@Resource
+	private RdfDao rdfDao;
 
-	/* (non-Javadoc)
-	 * @see edu.mayo.cts2.framework.service.profile.BaseService#getServiceDescription()
-	 */
-	@Override
-	public OpaqueData getServiceDescription() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	/* (non-Javadoc)
-	 * @see edu.mayo.cts2.framework.service.profile.BaseService#getServiceVersion()
-	 */
-	@Override
-	public String getServiceVersion() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	/* (non-Javadoc)
-	 * @see edu.mayo.cts2.framework.service.profile.BaseService#getServiceProvider()
-	 */
-	@Override
-	public SourceReference getServiceProvider() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see edu.mayo.cts2.framework.service.profile.BaseService#getSupportedFormatList()
-	 */
-	@Override
-	public List<FormatReference> getSupportedFormatList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see edu.mayo.cts2.framework.service.profile.BaseService#getDefaultFormat()
-	 */
-	@Override
-	public FormatReference getDefaultFormat() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see edu.mayo.cts2.framework.service.profile.BaseService#getKnownNamespaceList()
-	 */
-	@Override
-	public List<NamespaceReference> getKnownNamespaceList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	/* (non-Javadoc)
 	 * @see edu.mayo.cts2.framework.service.profile.QueryService#getResourceSummaries(edu.mayo.cts2.framework.service.profile.ResourceQuery, edu.mayo.cts2.framework.model.core.SortCriteria, edu.mayo.cts2.framework.model.command.Page)
@@ -100,8 +57,58 @@ public class BioportalRdfAssociationQueryService extends AbstractQueryService im
 	@Override
 	public DirectoryResult<AssociationDirectoryEntry> getResourceSummaries(
 			AssociationQuery query, SortCriteria sortCriteria, Page page) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String,Object> parameters = new HashMap<String,Object>();
+		parameters.put(LIMIT, page.getMaxToReturn()+1);
+		parameters.put(OFFSET, page.getStart());
+
+		String codeSystemVersionRestriction = "?codeSystemVersionRestriction";
+		
+		VariableQueryBuilder builder = new VariableQueryBuilder();
+		
+		if(query != null){
+			for(ResolvedFilter filter : query.getFilterComponent()){
+				ModelAttributeReference modelRef = filter.getModelAttributeReference();
+				
+				VariableTiedModelAttributeReference variableModelRef = this.findSupportedModelAttribute(modelRef);
+				
+				builder = builder.addQuery(variableModelRef.getVariable(), filter.getMatchValue());
+			}
+		}
+		
+		VariableQuery variableQuery = builder.build();
+		
+		parameters.put("filters", variableQuery);
+		
+		if(query != null && query.getRestrictions() != null) {
+			NameOrURI codeSystem = query.getRestrictions().getCodeSystemVersion();
+		
+			if(codeSystem != null){
+				if(StringUtils.isNotBlank(codeSystem.getName())){
+					CodeSystemName name = CodeSystemName.parse(codeSystem.getName());
+					
+					codeSystemVersionRestriction = name.getOntologyId();
+				}
+			}
+		}
+		
+		parameters.put("codeSystemVersionRestriction", codeSystemVersionRestriction);
+		
+		List<AssociationDirectoryEntry> results;
+		
+			results = rdfDao.selectForList(
+					ASSOCIATION_NAMESPACE, 
+					GET_ASSOCIATION_SUMMARIES,
+					parameters,
+					AssociationDirectoryEntry.class);
+		
+		boolean moreResults = results.size() > page.getMaxToReturn();
+		
+		if(moreResults){
+			results.remove(results.size() - 1);
+		}
+		
+		return new DirectoryResult<AssociationDirectoryEntry>(results,!moreResults);
+
 	}
 
 	/* (non-Javadoc)
@@ -123,18 +130,6 @@ public class BioportalRdfAssociationQueryService extends AbstractQueryService im
 		return 0;
 	}
 	
-
-
-
-
-	/* (non-Javadoc)
-	 * @see edu.mayo.cts2.framework.service.profile.BaseQueryService#getSupportedProperties()
-	 */
-	@Override
-	public Set<? extends PredicateReference> getSupportedProperties() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException();
-	}
 
 	/* (non-Javadoc)
 	 * @see edu.mayo.cts2.framework.service.profile.association.AssociationQueryService#getChildrenAssociationsOfEntity(edu.mayo.cts2.framework.service.profile.entitydescription.name.EntityDescriptionReadId, edu.mayo.cts2.framework.service.profile.entitydescription.EntityDescriptionQuery, edu.mayo.cts2.framework.model.command.ResolvedReadContext, edu.mayo.cts2.framework.model.command.Page)
---
+++ src/test/groovy/edu/mayo/cts2/framework/plugin/service/bprdf/profile/association/BioportalRdfAssociationQueryServiceTestIT.groovy
@@ -0,0 +1,40 @@
+package edu.mayo.cts2.framework.plugin.service.bprdf.profile.association
+
+import static org.junit.Assert.*
+
+import javax.annotation.Resource
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+import edu.mayo.cts2.framework.core.xml.Cts2Marshaller;
+import edu.mayo.cts2.framework.model.command.Page
+import edu.mayo.cts2.framework.plugin.service.bprdf.profile.association.BioportalRdfAssociationQueryService
+import edu.mayo.cts2.framework.service.profile.association.AssociationQuery
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value="classpath:/bioportal-rdf-service-test-context.xml")
+class BioportalRdfAssociationQueryServiceTestIT {
+	
+	@Resource
+	BioportalRdfAssociationQueryService query
+	
+	@Resource
+	Cts2Marshaller marshaller
+	
+	@Test
+	void TestGetResourceSummaries(){
+		def dir = query.getResourceSummaries(
+			[
+				getRestrictions:{null},
+				getFilterComponent:{[] as Set}
+			] as AssociationQuery,null,new Page())
+		
+		assertNotNull dir
+		assertTrue dir.getEntries().size() > 0
+	}
+	
+
+}
